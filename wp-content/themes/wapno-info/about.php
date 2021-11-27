4d24fc53f80f879ad4af2e53352e6bd660385c2b
d3ntka <77463886+d3ntka@users.noreply.github.com>
2021-11-27 21:54:26 +0100

    about dev

--- wp-content/themes/wapno-info/about.php
+++ wp-content/themes/wapno-info/about.php
@@ -28,8 +28,82 @@ get_template_part('components/breadcrumbs/breadcrumbs');
             <?php endif; ?>
         </div>
 
+        <div class="container--front">
+            <div class="container">
+            <!-- Nasze cele -->
+            <div class="row">
+                <?php if ( $section_2_title = get_field( 'section_2_title' ) ) : ?>
+                    <h2>
+                        <?php echo esc_html( $section_2_title ); ?>
+                    </h2>
+                <?php endif; ?>     
+                <?php if ( $section_2_list = get_field( 'section_2_list' ) ) : ?>
+                    <?php echo $section_2_list; ?>
+                <?php endif; ?>
+            </div>
 
+            <?php if ( have_rows( 'teams' ) ) : ?>
+                <div class="teams">
+                    <?php while ( have_rows( 'teams' ) ) :
+                        the_row(); ?>
+                        <div class="team">
+                            <?php
+                            $teams_icon = get_sub_field( 'teams_icon' );
+                            if ( $teams_icon ) : ?>
+                                <img src="<?php echo esc_url( $teams_icon['url'] ); ?>" alt="<?php echo esc_attr( $teams_icon['alt'] ); ?>" />
+                            <?php endif; ?>
+        
+                            <?php if ( $teams_title = get_sub_field( 'teams_title' ) ) : ?>
+                                <h2>
+                                    <?php echo esc_html( $teams_title ); ?>
+                                </h2>
+                            <?php endif; ?>
+        
+                            <?php if ( $teams_txt = get_sub_field( 'teams_txt' ) ) : ?>
+                                <?php echo esc_html( $teams_txt ); ?>
+                            <?php endif; ?>
+                        </div>
+    
+                    <?php endwhile; ?>
+                </div>
+            <?php endif; ?>
+            
+            <div class="d-flex">
+                <?php if ( $associations_title = get_field( 'associations_title' ) ) : ?>
+                    <h3>
+                        <?php echo esc_html( $associations_title ); ?>
+                    </h3>
+                <?php endif; ?>
+                <?php if ( $associations_txt = get_field( 'associations_txt' ) ) : ?>
+                    <?php echo esc_html( $associations_txt ); ?>
+                <?php endif; ?>
+            </div>
 
+            <?php if ( have_rows( 'associations_international' ) ) : ?>
+                <div class="row">
+                    <?php while ( have_rows( 'associations_international' ) ) :
+                        the_row(); ?>
+                        <div class="col-md-6 col-xxl-3">
+                            <?php
+                            $associations_inter_logo = get_sub_field( 'associations_inter_logo' );
+                            if ( $associations_inter_logo ) : ?>
+                                <div class="">
+                                    <img src="<?php echo esc_url( $associations_inter_logo['url'] ); ?>" alt="<?php echo esc_attr( $associations_inter_logo['alt'] ); ?>" />
+                                </div>
+                            <?php endif; ?>
+                                
+                            <?php if ( $associations_inter_txt = get_sub_field( 'associations_inter_txt' ) ) : ?>
+                                <div class="">
+                                    <?php echo esc_html( $associations_inter_txt ); ?>
+                                </div>
+                            <?php endif; ?>
+                        </div>
+                            
+                    <?php endwhile; ?>
+                </div>
+            <?php endif; ?>
+            </div>
+        </div>
 
         
         
@@ -37,7 +111,7 @@ get_template_part('components/breadcrumbs/breadcrumbs');
             <div class="container">
                 <?php if ($producers_section_title = get_field('producers_section_title')) : ?>
                     <h2 class="big producers__sect--title">
-                        <?php echo esc_html($producers_section_title); ?>
+                        <?php echo $producers_section_title; ?>
                     </h2>
                 <?php endif; ?>
 
@@ -65,16 +139,6 @@ get_template_part('components/breadcrumbs/breadcrumbs');
                                                     <?php echo $producer_txt; ?>
                                                 </div>
                                             <?php endif; ?>
-
-                                            <?php
-                                            $link = get_sub_field('producer_link');
-                                            if ($link) :
-                                                $link_url = $link['url'];
-                                                $link_title = $link['title'];
-                                                $link_target = $link['target'] ? $link['target'] : '_blank';
-                                            ?>
-                                                <a class="producer__btn btn btn-primary button" href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
-                                            <?php endif; ?>
                                         </div>
 
                                     </div>
@@ -89,6 +153,52 @@ get_template_part('components/breadcrumbs/breadcrumbs');
         </div>
 
     </div>
+
+
+    <div class="container--front">
+        <div class="amonit">
+            <div class="container">
+                <div class="row justify-content-end">
+                    <div class="col-md-6">
+                        <?php if ( $amonit_title = get_field( 'amonit_title' ) ) : ?>
+                            <h2>
+                                <?php echo esc_html( $amonit_title ); ?>
+                            </h2>
+                        <?php endif; ?>
+                        <?php if ( $amonit_txt = get_field( 'amonit_txt' ) ) : ?>
+                            <?php echo $amonit_txt; ?>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+        <?php if ( $history_title = get_field( 'history_title' ) ) : ?>
+            <?php echo esc_html( $history_title ); ?>
+        <?php endif; ?>
+        <?php if ( $history_txt = get_field( 'history_txt' ) ) : ?>
+            <?php echo $history_txt; ?>
+        <?php endif; ?>
+
+        <?php if ( have_rows( 'history_rptr' ) ) : ?>
+            <?php while ( have_rows( 'history_rptr' ) ) :
+                the_row(); ?>
+                
+                <?php if ( $history_rptr_title = get_sub_field( 'history_rptr_title' ) ) : ?>
+                    <?php echo esc_html( $history_rptr_title ); ?>
+                <?php endif; ?>
+
+                <?php if ( $history_rptr_txt = get_sub_field( 'history_rptr_txt' ) ) : ?>
+                    <?php echo $history_rptr_txt; ?>
+                <?php endif; ?>
+
+            <?php endwhile; ?>
+        <?php endif; ?>
+
+        </div>
+    </div>
+
+
 
 
     <div class="container--front">
