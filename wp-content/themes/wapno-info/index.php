ca05d210b0439f524b2b2b58b78f699039b4afc1
d3ntka <77463886+d3ntka@users.noreply.github.com>
2021-11-28 23:08:46 +0100

    fix: index container

--- wp-content/themes/wapno-info/index.php
+++ wp-content/themes/wapno-info/index.php
@@ -9,19 +9,21 @@ get_header();
 
 $page_id = get_option( 'page_for_posts' );
 ?>
-<div class="row padding-cta">
-	<div class="col-md-12">
-		<?php
-			echo apply_filters( 'the_content', get_post_field( 'post_content', $page_id ) );
-
-			edit_post_link( __( 'Edit', 'wapno-info' ), '<span class="edit-link">', '</span>', $page_id );
-		?>
-	</div><!-- /.col -->
-	<div class="col-md-12">
-		<?php
-			get_template_part( 'archive', 'loop' );
-		?>
-	</div><!-- /.col -->
-</div><!-- /.row -->
+<div class="container">
+	<div class="row padding-cta">
+		<div class="col-md-12">
+			<?php
+				echo apply_filters( 'the_content', get_post_field( 'post_content', $page_id ) );
+	
+				edit_post_link( __( 'Edit', 'wapno-info' ), '<span class="edit-link">', '</span>', $page_id );
+			?>
+		</div><!-- /.col -->
+		<div class="col-md-12">
+			<?php
+				get_template_part( 'archive', 'loop' );
+			?>
+		</div><!-- /.col -->
+	</div><!-- /.row -->
+</div>
 <?php
 get_footer();
